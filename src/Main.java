f511b05cffd94c7babd79fa07b2c2f600db94b89
Michael Hansen <michaelhansenit@live.dk>
2018-09-25 14:13:44 +0200

    Linje med punkter testet.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        double x = 3;
+        double y = 0;
+        Punkt punktA = new Punkt(x, y);
+        double x2 = 0;
+        double y2 = 4;
+        Punkt punktB = new Punkt(x2, y2);
+
+        Linje linje1 = new Linje();
+        double length = linje1.length(x, y, x2, y2);
+        System.out.println("Længden på linjen er på: " + length);
     }
 }
